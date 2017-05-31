311d01ea94fe2b2caee1a72707216a413e4de048
Slawomir Radzyminski <s.radzyminski@MP18570.local>
2017-05-31 21:58:11 +0200

    New blog post

---
+++ src/test/java/gui/selectors/tests/BlogTest.java
@@ -0,0 +1,37 @@
+package gui.selectors.tests;
+
+import gui.selectors.pages.HomePage;
+import org.fluentlenium.adapter.testng.FluentTestNg;
+import org.fluentlenium.core.annotation.Page;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * See more at http://www.awesome-testing.com/2017/05/how-to-find-test-cssselector-using.html
+ */
+public class BlogTest extends FluentTestNg {
+
+    private static final int EXPECTED_NUMBER_OF_POSTS = 5;
+
+    @Page
+    private HomePage homePage;
+
+    @Override
+    public WebDriver newWebDriver() {
+        return new ChromeDriver();
+    }
+
+    @Test
+    public void shouldDisplayHeader() {
+        homePage.go();
+        homePage.assertThatHeaderIsDisplayed();
+    }
+
+    @Test
+    public void shouldBeFivePosts() {
+        homePage.go();
+        homePage.assertThatNumberOFTitlesEquals(EXPECTED_NUMBER_OF_POSTS);
+    }
+
+}
