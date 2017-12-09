fec306d18285dc79f6fa24dc80e620c3871d46b4
slawekradzyminski <slawomir.radzyminski@gmail.com>
2017-12-09 10:44:48 +0100

    Added WireMock post code

---
+++ src/test/java/api/wiremock/test/BasicPostTest.java
@@ -0,0 +1,44 @@
+package api.wiremock.test;
+
+import api.wiremock.util.WireMockTest;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.json.Json;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.apache.http.HttpStatus.SC_OK;
+
+public class BasicPostTest extends WireMockTest {
+
+    @Before
+    public void setUp() {
+        stubFor(post(urlEqualTo("/addPerson"))
+                .withHeader("Accept", equalTo(JSON.toString()))
+                .withRequestBody(equalToJson(getUserJson()))
+                .willReturn(ok()));
+    }
+
+    private String getUserJson() {
+        return Json.createObjectBuilder()
+                .add("firstName", "Slawomir")
+                .add("lastName", "Radzyminski")
+                .add("age", "18")
+                .build().toString();
+    }
+
+    @Test
+    public void ipTest() {
+        given()
+                .contentType(JSON)
+                .header("Accept", JSON.toString())
+                .body(getUserJson())
+                .when()
+                .post("/addPerson")
+                .then()
+                .statusCode(SC_OK);
+    }
+
+}
