46f8b673a3a606b062b2c5950ffc4371ff28152b
Slawek <s.radzyminski@ocado.com>
2019-05-21 16:38:17 +0200

    Code for JMH microbenchmarking post

---
+++ src/main/java/com/awesome/testing/performance/javabenchmark/BenchMark.java
@@ -0,0 +1,60 @@
+package com.awesome.testing.performance.javabenchmark;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+@Fork(value = 2, warmups = 1)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 1)
+public class BenchMark {
+
+    private static final long N = 12000000L;
+
+    @Benchmark
+    public long sequentialSum() {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(N)
+                .reduce(0L, Long::sum);
+    }
+
+    @Benchmark
+    public long parallelSum() {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(N)
+                .parallel()
+                .reduce(0L, Long::sum);
+    }
+
+    @Benchmark
+    public long iterativeSum() {
+        long result = 0;
+        for (long i = 1L; i <= N; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+    @Benchmark
+    public long rangedSum() {
+        return LongStream.rangeClosed(1, N)
+                .reduce(0L, Long::sum);
+    }
+
+    @Benchmark
+    public long parallelRangedSum() {
+        return LongStream.rangeClosed(1, N)
+                .parallel()
+                .reduce(0L, Long::sum);
+    }
+
+
+}
