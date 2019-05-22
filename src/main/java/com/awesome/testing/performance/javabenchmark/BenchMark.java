e508ec043ec2a9ae497a042c95b0ffe90e0fcb5b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2019-05-22 19:59:13 +0200

    Benchmarks

--- src/main/java/com/awesome/testing/performance/javabenchmark/BenchMark.java
+++ src/main/java/com/awesome/testing/performance/javabenchmark/BenchMark.java
@@ -3,24 +3,23 @@ package com.awesome.testing.performance.javabenchmark;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
 
 import java.util.concurrent.TimeUnit;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
-@State(Scope.Benchmark)
-@Fork(value = 5, warmups = 2, jvmArgs={"-Xms4G", "-Xmx4G"})
+@Fork(value = 3, warmups = 0)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 5, time = 10)
+@Measurement(iterations = 5, time = 10)
 public class BenchMark {
 
-    private static final long N = 12000000L;
+    private static final long N = 20000000L;
 
     @Benchmark
     public long sequentialSum() {
@@ -59,8 +58,4 @@ public class BenchMark {
                 .reduce(0L, Long::sum);
     }
 
-    @TearDown(Level.Invocation)
-    public void tearDown() {
-        System.gc();
-    }
 }
---
+++ src/main/java/com/awesome/testing/performance/javabenchmark/setjava8.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8.0_212`
